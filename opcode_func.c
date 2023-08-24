f97ecf6f4f012a5465e10250314780ab4223b1ac
fawazabdganiyu <fawazabdganiyu@gmail>
2023-08-24 09:31:00 +0100

    Implement expected div opcode

--- opcode_func.c
+++ opcode_func.c
@@ -1,5 +1,6 @@
 #include "monty.h"
 
+void divide(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
@@ -8,6 +9,39 @@ void pop(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
+
+/**
+ * divide - divides the second top element of the stack by
+ *	the top element of the stack.
+ *
+ * @line_number: The line number where the opcode was found
+ * @stack: A pointer to the stack list pointer
+ */
+void divide(stack_t **stack, unsigned int line_number)
+{
+	int result = 0;
+	stack_t *temp;
+
+	if (!*stack || !(*stack)->next)
+	{
+		dprintf(2, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		dprintf(2, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	result = (*stack)->next->n / (*stack)->n;
+
+	(*stack)->next->n = result;
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(temp);
+}
 
 /**
  * sub - subtracts the top element of the stack from
